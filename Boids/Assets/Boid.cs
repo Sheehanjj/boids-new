e3b5d042c95dac3d20c5c3293c1ddda64f7ff7b0
John Sheehan <sheehanj@sunyulster.edju>
2018-04-05 11:47:36 -0400

    Initial Version from 4-5-18 Class

---
+++ Boids/Assets/Boid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+	public Vector3 pos {
+		get { return transform.position; }
+		set { transform.position = value; }
+	}
+
+	void FixedUpdate(){
+		Vector3 vel = rigid.velocity;
+		Spawner spn = Spawner.S;
+
+		Vector3 delta = Atrractor.POS - pos;
+		bool attracted = (delta.magnitude > spn.attractPushDist);
+		Vector3 velAttract = delta.normalized * spn.velocity;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Boids/Assets/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	static public Spawner		S;
+	static public List<Boid>	boids;
+
+	[Header("Set in Inspector: Spawning")]
+	public GameObject	boidPrefab;
+	public Transform	boidAnchor;
+	public int			numBoids = 100;
+	public float		spawnRadius = 100f;
+	public float		spawnDelay = 0.1f;
+
+	void Awake () {
+		S = this;
+		boids = new List<Boid>();
+		InstantiateBoid();
+	}
+
+	public void InstantiateBoid(){
+		GameObject go = Instantiate (boidPrefab);
+		Boid b = go.GetComponent<Boid> ();
+		b.transform.SetParent (boidAnchor);
+		boids.Add (b);
+		if (boids.Count < numBoids){
+			Invoke ("InstantiateBoid", spawnDelay);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
