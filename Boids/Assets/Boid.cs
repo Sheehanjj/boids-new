0112ace4170e88120c3d35ff6f1196a18104a822
John Sheehan <sheehanj@sunyulster.edju>
2018-04-10 11:08:47 -0400

    Updated Version from 4-10-18 Class

--- Boids/Assets/Boid.cs
+++ Boids/Assets/Boid.cs
@@ -4,19 +4,64 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour {
 
-	public Vector3 pos {
-		get { return transform.position; }
-		set { transform.position = value; }
+	[Header("Set Dynamically")]
+	public Rigidbody	rigid;
+
+	void Awake(){
+		rigid = GetComponent<Rigidbody> ();
+
+		// Set a random initial position
+		pos = Random.insideUnitSphere * Spawner.S.spawnRadius;
+
+		// Set a random initial velocity
+		Vector3 vel = Random.onUnitSphere * Spawner.S.velocity;
+		rigid.velocity = vel;
+
+		LookAhead ();
+
+		// Give each Boid a Random Color
+		Color randColor = Color.black;
+		while (randColor.r + randColor.g + randColor.b < 1.0f) {
+			randColor = new Color (Random.value, Random.value, Random.value);
+		}
+		Renderer[] rends = gameObject.GetComponentsInChildren<Renderer> ();
+		foreach (Renderer r in rends) {
+			r.material.color = randColor;
+		}
+		TrailRenderer tRend = GetComponent<TrailRenderer> ();
+		tRend.material.SetColor ("_TintColor", randColor);
+	}
+
+	void LookAhead(){
+		// orients the Boid to look in the direction that it is flying
+		transform.LookAt (pos + rigid.velocity);
 	}
 
 	void FixedUpdate(){
 		Vector3 vel = rigid.velocity;
 		Spawner spn = Spawner.S;
 
-		Vector3 delta = Atrractor.POS - pos;
+		// Attract the Boid to the Attractor
+		Vector3 delta = Attractor.POS - pos;
+
+		// Is the Boid already moving towards the Attractor
 		bool attracted = (delta.magnitude > spn.attractPushDist);
 		Vector3 velAttract = delta.normalized * spn.velocity;
+
+		float fdt = Time.fixedDeltaTime;
+
+		if (attracted) {
+			vel = Vector3.Lerp (vel, velAttract, spn.attractPull * fdt);
+		} else {
+			vel = Vector3.Lerp (vel, -velAttract, spn.attractPush * fdt);
+		}
+
+		vel = vel.normalized * spn.velocity;
+		rigid.velocity = vel;
+
+		LookAhead ();
 	}
+
 	// Use this for initialization
 	void Start () {
 		
@@ -25,5 +70,10 @@ public class Boid : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public Vector3 pos {
+		get { return transform.position; }
+		set { transform.position = value; }
 	}
 }
--- Boids/Assets/Spawner.cs
+++ Boids/Assets/Spawner.cs
@@ -14,6 +14,17 @@ public class Spawner : MonoBehaviour {
 	public float		spawnRadius = 100f;
 	public float		spawnDelay = 0.1f;
 
+	// These fields allow you to adjust the flocking behavoir of the Boids
+	public float		velocity = 30f;
+	public float		neighborDist = 30f;
+	public float 		collDist = 4f;
+	public float		velMatching = 0.25f;
+	public float		flockCentering = 0.2f;
+	public float		collAvoid = 2f;
+	public float		attractPull = 2f;
+	public float		attractPush = 2f;
+	public float		attractPushDist = 5f;
+
 	void Awake () {
 		S = this;
 		boids = new List<Boid>();
